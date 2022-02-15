62d6f6d291eef7984e8b4343ac6389678cd56009
iakshatgupta <95222515+iakshatgupta@users.noreply.github.com>
2022-02-15 05:44:21 +0530

    Add files via upload
    Ques 1- Floor Assembly
    Ques 2- BST

---
+++ FloorAssembly/Main.java
@@ -0,0 +1,12 @@
+package com.gl.FloorAssembly;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		service in=new service();
+		in.inputFromUser();
+		in.constructionOrder();
+
+	}
+}
